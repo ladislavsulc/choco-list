0cac7344d06b8a0c224ce2c1debaae53a2aac99e
Ladislav Šulc <ladislav.sulc@gmail.com>
2024-03-24 14:12:04 +0100

    package update

--- execute.sh
+++ execute.sh
@@ -3,10 +3,10 @@
 echo "Listing all Chocolatey packages, importing to README.md..."
 
 # List all Chocolatey packages, import them and format them into README.md
-choco list -li | awk '{print "- "$0}' > README.md
+choco list | awk '{print "- "$0}' > README.md
 
 # List all Choco packages in one row without versions and write it in packages.md
-choco list -lo | sed 's/[0-9]//g' | sed 's/\.//g' > packages.md
+choco list | sed 's/[0-9]//g' | sed 's/\.//g' > packages.md
 
 echo "Prepending the repo title..."
 
