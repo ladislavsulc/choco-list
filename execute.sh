1d0e1700d303fc2f12ba8e404590dd184046d046
Ladislav Šulc <ladislav.sulc@gmail.com>
2024-09-01 23:36:41 +0200

    Update

--- execute.sh
+++ execute.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 
-# List all installed Chocolatey packages
-choco list -l -r | awk '$1 ~ /^[a-zA-Z]/ {print $1}' | sort | uniq -d > temp.txt
-
-# Read the duplicate packages and keep the original name
-while IFS= read -r pkg
-do
-  echo -n "$pkg "
-done < temp.txt > packages_temp.md
-
-# Remove duplicate packages
-cat packages_temp.md packages.md | tr '\n' ' ' | sed 's/ *$//' > packages.md
-
-rm temp.txt packages_temp.md
+choco list | sort | uniq > choco-packages.md
 
